e8e29cde5beb751f0d6e20c117a406b940639173
Tiago Jerônimo <tiago.batmansz@gmail.com>
2022-03-18 19:02:55 -0300

    Requisito 5 - Feito

--- src/App.js
+++ src/App.js
@@ -11,20 +11,74 @@ class App extends React.Component {
       cardImage: '',
       cardAttr1: '0',
       cardAttr2: '0',
-      cardAttr3: '',
+      cardAttr3: '0',
       cardRare: '',
       cardTrunfo: false,
       isSaveButtonDisabled: true,
     };
+    this.validationForms = this.validationForms.bind(this);
     this.onInputChange = this.onInputChange.bind(this);
+    this.validacaoNumbers = this.validacaoNumbers.bind(this);
+    this.validacaoNumbersNegativos = this.validacaoNumbersNegativos.bind(this);
+    this.validacao = this.validacao.bind(this);
+    this.validacaoNumbersMaiores = this.validacaoNumbersMaiores.bind(this);
   }
 
   onInputChange({ target }) {
     const { name } = target;
     const value = target.type === 'checked' ? target.checked : target.value;
-    this.setState({ [name]: value });
-    const { cardName } = this.state;
-    if (cardName !== '') {
+    this.setState({ [name]: value },
+      () => this.validationForms());
+  }
+
+  validacaoNumbers() {
+    const limite = 90;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    if (cardAttr1 > limite || cardAttr2 > limite || cardAttr3 > limite) {
+      return true;
+    }
+    return false;
+  }
+
+  validacaoNumbersNegativos() {
+    const minimo = 0;
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    if (cardAttr1 < minimo || cardAttr2 < minimo || cardAttr3 < minimo) {
+      return true;
+    }
+    return false;
+  }
+
+  validacaoNumbersMaiores() {
+    const { cardAttr1, cardAttr2, cardAttr3 } = this.state;
+    const maximo = 210;
+    const somatoriaNumeros = (+cardAttr1 + +cardAttr2 + +cardAttr3);
+    if (somatoriaNumeros <= maximo) {
+      return false;
+    }
+    return true;
+  }
+
+  validacao() {
+    const { cardName, cardDescription, cardImage } = this.state;
+    if (cardName !== '' && cardDescription !== '' && cardImage !== '') {
+      return false;
+    }
+    return true;
+  }
+
+  // SE TODAS RETORNAREM FALSE: isSaveButtonDisabled vai ficar false
+  // SE TODAS RETORNAREM TRUE : isSaveButtonDisabled vai ficar TRUE
+  // se alguma retornar false: isSaveButtonDisabled vai ficar false
+  // Se alguma retornar true: isSaveButtonDisabled vai ficar false
+
+  validationForms() {
+    if (this.validacao()
+    || this.validacaoNumbers()
+    || this.validacaoNumbersNegativos()
+    || this.validacaoNumbersMaiores()) {
+      this.setState({ isSaveButtonDisabled: true });
+    } else {
       this.setState({ isSaveButtonDisabled: false });
     }
   }
